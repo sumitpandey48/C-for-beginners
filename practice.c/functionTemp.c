1cbf0f7e95419ff9aacaa66088b92b13574931f1
sumitpandey48 <pandeysumit28130@gmail.com>
2025-04-25 23:42:02 +0530

    create function and recursion in C

---
+++ practice.c/functionTemp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+float convertTemp(float celsius);
+int main(){
+    float far = convertTemp(37);
+    printf("far : %f", far);
+    return 0;
+}
+
+float convertTemp(float celsius){
+float far = celsius * (9/5) + 32;
+return far;
+}
