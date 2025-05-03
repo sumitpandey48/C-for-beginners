35b410f71515cd45ce0819d4268f13922eac7f93
sumitpandey48 <pandeysumit28130@gmail.com>
2025-05-03 23:53:03 +0530

    Create Array in C

---
+++ practice.c/Array.c/2Darray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    // 2 x 3
+    int marks[2][3];
+    marks[0][0] = 90;
+    marks[0][1] = 89;
+    marks[0][2] = 78;
+
+    marks[1][0] = 90;
+    marks[1][1] = 89;
+    marks[1][2] = 78;
+
+    printf("%d\n",marks[0][1]);
+
+    return 0;
+}
+
+
+
+
